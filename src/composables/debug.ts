1c4fff74e2a6f294d269ec44cd82181a75926556
CodeAdraas <67322512+CodeAdraas@users.noreply.github.com>
2026-04-18 23:52:18 +0200

    Rewrite debug composables

--- src/composables/debug.ts
+++ src/composables/debug.ts
@@ -1,7 +1,28 @@
-export const useLog = (msg: any, ...objs: any[]) => {
-  console.log(msg, ...objs);
+const logger = {
+  log(...objs: any[]) {
+    console.log('[DEBUG] ', ...objs)
+  },
+  info(...objs: any[]) {
+    console.log('[INFO] ', ...objs)
+  },
+  warn(...objs: any[]) {
+    console.warn('[WARN] ', ...objs)
+  },
+  error(...objs: any[]) {
+    console.error('[ERROR] ', ...objs)
+  }
 }
 
+export const useLogger = () => {
+  return logger;
+}
+
+/** @deprecated */
+export const useLog = (msg: any, ...objs: any[]) => {
+  return console;
+}
+
+/** @deprecated */
 export const useLogError = (msg: any, ...objs: any[]) => {
   console.error(msg, ...objs);
 }
